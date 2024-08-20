100ba43e118f3bde6fc7ef6366abf718e3a61343
Elessa Fermano <elessamaefermano@gmail.com>
2024-08-20 23:34:24 +0800

    commit

--- app/Models/category.php
+++ app/Models/category.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
-    protected $fillable = [
-        'name',
-        'description',
-    ];
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
